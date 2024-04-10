6bab4cc104c1154a6ea9b92ac9a8603b3c61e5f7
keni3 <keni.3nimu@gmail.com>
2024-04-10 15:16:12 +0900

    パイプライン用のスクリプトファイルとバッチファイルの作成

---
+++ apps/jenkins/build.bat
@@ -0,0 +1,18 @@
+set IAR_BUILD_PATH=C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\
+set MCLD4P_PATH=C:\ProgramData\Jenkins\.jenkins\workspace\%JOB_NAME%\apps\ide\pca10040\s132\iar\
+C:
+cd %IAR_BUILD_PATH%
+
+if not exist %MCLD4P_PATH%Debug\Exe\ mkdir %MCLD4P_PATH%Debug\Exe\
+
+IarBuild.exe %MCLD4P_PATH%ble_app_uart_pca10040_s132.ewp -clean nrf52832_xxaa
+IarBuild.exe %MCLD4P_PATH%ble_app_uart_pca10040_s132.ewp -build nrf52832_xxaa
+
+if exist %MCLD4P_PATH%_build\ble_app_uart_pca10040_s132.hex (
+    move %MCLD4P_PATH%_build\ble_app_uart_pca10040_s132.hex %MCLD4P_PATH%Debug\Exe
+    IarBuild.exe %MCLD4P_PATH%ble_app_uart_pca10040_s132.ewp -clean nrf52832_xxaa
+    exit 0
+) else (
+    IarBuild.exe %MCLD4P_PATH%ble_app_uart_pca10040_s132.ewp -clean nrf52832_xxaa
+    exit 1
+)
---
+++ apps/jenkins/unit_test.bat
@@ -0,0 +1,5 @@
+set MCLD4P_PATH=C:\ProgramData\Jenkins\.jenkins\workspace\%JOB_NAME%\apps\
+C:
+cd %MCLD4P_PATH%
+
+ceedling
