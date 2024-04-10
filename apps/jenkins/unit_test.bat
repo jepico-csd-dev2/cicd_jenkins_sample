50d4d0da0dd41aa580e28660a60e990c1e77b20f
keni3 <keni.3nimu@gmail.com>
2024-04-10 15:27:08 +0900

    バッチファイルのパス修正

--- apps/jenkins/unit_test.bat
+++ apps/jenkins/unit_test.bat
@@ -1,4 +1,4 @@
-set MCLD4P_PATH=C:\ProgramData\Jenkins\.jenkins\workspace\%JOB_NAME%\apps\
+set MCLD4P_PATH=C:\Users\jepic\AppData\Local\Jenkins\.jenkins\workspace\%JOB_NAME\apps\
 C:
 cd %MCLD4P_PATH%
 
