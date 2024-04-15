a22fdb3bab66187f2a4f2fac3da66ab69714636d
keni3 <keni.3nimu@gmail.com>
2024-04-15 10:07:58 +0900

    バッチファイルを更新

--- apps/jenkins/build.bat
+++ apps/jenkins/build.bat
@@ -3,16 +3,13 @@ set MCLD4P_PATH=C:\Users\jepic\AppData\Local\Jenkins\.jenkins\workspace\%JOB_NAM
 C:
 cd %IAR_BUILD_PATH%
 
-if not exist %MCLD4P_PATH%Debug\Exe\ mkdir %MCLD4P_PATH%Debug\Exe\
+
 
 IarBuild.exe %MCLD4P_PATH%ble_app_uart_pca10040_s132.ewp -clean nrf52832_xxaa
 IarBuild.exe %MCLD4P_PATH%ble_app_uart_pca10040_s132.ewp -build nrf52832_xxaa
 
 if exist %MCLD4P_PATH%_build\ble_app_uart_pca10040_s132.hex (
-    move %MCLD4P_PATH%_build\ble_app_uart_pca10040_s132.hex %MCLD4P_PATH%Debug\Exe
-    IarBuild.exe %MCLD4P_PATH%ble_app_uart_pca10040_s132.ewp -clean nrf52832_xxaa
     exit 0
 ) else (
-    IarBuild.exe %MCLD4P_PATH%ble_app_uart_pca10040_s132.ewp -clean nrf52832_xxaa
     exit 1
 )
--- apps/jenkins/deploy.bat
+++ apps/jenkins/deploy.bat
@@ -1,1 +1,11 @@
-echo 'Deploy'
+set MCLD4P_PATH=C:\Users\jepic\AppData\Local\Jenkins\.jenkins\workspace\%JOB_NAME%\apps\ide\pca10040\s132\iar\
+
+if not exist %MCLD4P_PATH%Debug\Exe\ mkdir %MCLD4P_PATH%Debug\Exe\
+
+
+if exist %MCLD4P_PATH%_build\ble_app_uart_pca10040_s132.hex (
+    move %MCLD4P_PATH%_build\ble_app_uart_pca10040_s132.hex %MCLD4P_PATH%Debug\Exe\ble_app_uart_pca10040_s132_%BUILD_NUMBER%_%BUILD_ID%.hex
+    exit 0
+) else (
+    exit 1
+)
